d567625cb5e7779bfa10227977b3eb14784993cf
ssartell <ssartell@rightpoint.com>
2015-02-09 22:24:52 -0600

    update

--- responsive-banner/script.js
+++ responsive-banner/script.js
@@ -1,11 +1,43 @@
 (function() {
+	ko.extenders.numeric = function(target, precision) {
+	    //create a writable computed observable to intercept writes to our observable
+	    var result = ko.pureComputed({
+	        read: target,  //always return the original observables value
+	        write: function(newValue) {
+	        	var current = target(),
+	        	roundingMultiplier = Math.pow(10, precision),
+	        	newValueAsNum = isNaN(newValue) ? 0 : parseFloat(+newValue),
+	        	valueToWrite = Math.round(newValueAsNum * roundingMultiplier) / roundingMultiplier;
+
+	            //only write if it changed
+	            if (valueToWrite !== current) {
+	            	target(valueToWrite);
+	            } else {
+	                //if the rounded value is the same, but a different value was written, force a notification for the current field
+	                if (newValue !== current) {
+	                	target.notifySubscribers(valueToWrite);
+	                }
+	            }
+	        }
+	    }).extend({ notify: 'always' });
+
+	    //initialize with current value to make sure it is rounded appropriately
+	    result(target());
+
+	    //return the new computed observable
+	    return result;
+	};
+}())
+
+
+(function() {
 
 	var CalculatorViewModel = function() {
 		var self = this;
 
-		self.imageSize = ko.observable(0);
-		self.focalSize = ko.observable(0);
-		self.leftEdge = ko.observable(0);
+		self.imageSize = ko.observable(0).extend({ numeric: 0 });
+		self.focalSize = ko.observable(0).extend({ numeric: 0 });
+		self.leftEdge = ko.observable(0).extend({ numeric: 0 });
 
 		self.halfway = ko.computed(function() {
 			return self.imageSize() / 2;
@@ -49,4 +81,5 @@
 	};
 
 	ko.applyBindings(new CalculatorViewModel());
+
 }());
