9dac1033c211a19d4be2ad2bb58b378b1c3e1af5
ssartell <ssartell@rightpoint.com>
2018-03-27 13:20:57 -0500

    update

--- webgl/index.js
+++ webgl/index.js
@@ -2529,7 +2529,7 @@ function initBuffers(gl) {
 	gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
 
 	const positions = [
-		1.0, 1.0,
+		0.95, 1.0,
 		-1.0, 1.0,
 		1.0, -1.0,
 		-1.0, -1.0,
@@ -2547,7 +2547,7 @@ function initBuffers(gl) {
 
 
 function drawScene(gl, programInfo, buffers) {
-	gl.clearColor(0.0, 0.0, 0.0, 1.0);  // Clear to black, fully opaque
+	gl.clearColor(1.0, 0.0, 0.0, 1.0);  // Clear to black, fully opaque
 	gl.clearDepth(1.0);                 // Clear everything
 	gl.enable(gl.DEPTH_TEST);           // Enable depth testing
 	gl.depthFunc(gl.LEQUAL);            // Near things obscure far things
